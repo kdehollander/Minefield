f85f6e3bdcef2f9960a698ab8ced106be4750de1
kdehollander <kdehollander@mail.csuchico.edu>
2016-09-26 11:50:03 -0700

    Could not make. Added tests, did not run to check correctness or to add to buglog.md

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -15,10 +15,88 @@ class FieldTest : public ::testing::Test
 		virtual void TearDown(){}
 };
 
+TEST(FieldTest, checkInBounds)
+{
+	Field minefield;
+
+	ASSERT_TRUE(minefield.inBounds(5, 5));
+	ASSERT_FALSE(minefield.inBounds(12, 5));
+	ASSERT_FALSE(minefield.inBounds(5, 12));
+	ASSERT_TRUE(minefield.inBounds(0, 0));
+	ASSERT_TRUE(minefield.inBounds(5, 0));
+	ASSERT_TRUE(minefield.inBounds(0, 5));
+	ASSERT_FALSE(minefield.inBounds(-1, 5));
+	ASSERT_FALSE(minefield.inBounds(5, -1));
+	
+}
+
+TEST(FieldTest, checkInBoundsResized)
+{
+	Field minefield(20);
+
+	ASSERT_TRUE(minefield.inBounds(15, 15));
+	ASSERT_FALSE(minefield.inBounds(22, 5));
+	ASSERT_FALSE(minefield.inBounds(5, 22));
+	ASSERT_TRUE(minefield.inBounds(0, 0));
+	ASSERT_TRUE(minefield.inBounds(15, 0));
+	ASSERT_TRUE(minefield.inBounds(0, 15));
+	ASSERT_FALSE(minefield.inBounds(-1, 5));
+	ASSERT_FALSE(minefield.inBounds(5, -1));
+	
+}
+
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
 	
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+
+	minefield.placeMine(0, 5);
+	ASSERT_EQ(MINE_HIDDEN, minefile.get(0,5));
+
+	minefield.placeMine(5, 0);
+	ASSERT_EQ(MINE_HIDDEN, minefile.get(5,0));
+
+	minefield.placeMine(0, 0);
+	ASSERT_EQ(MINE_HIDDEN, minefile.get(0,0));
+
+	minefield.placeMine(12, 5);
+	ASSERT_ANY_THROW(minefile.get(12,5));
+
+	minefield.placeMine(5, 12);
+	ASSERT_ANY_THROW(minefile.get(5,12));
+
+	minefield.placeMine(12, 12);
+	ASSERT_ANY_THROW(minefile.get(12,12));
+
+	minefield.placeMine(-1, 5);
+	ASSERT_ANY_THROW(minefile.get(-1,5));
+
+	minefield.placeMine(5, -1);
+	ASSERT_ANY_THROW(minefile.get(5,-1));
+
+	minefield.placeMine(-1, -1);
+	ASSERT_ANY_THROW(minefile.get(-1,-1));
+}
+TEST(FieldTest, checkSafe)
+{
+	Field minefield;
+
+	minefield.placeMine(5,5);
+	minefield.placeMine(0,0);
+	minefield.placeMine(0,5);
+	minefield.placeMine(5,0);
+
+        ASSERT_TRUE(minefield.isSafe(4,5));
+        ASSERT_TRUE(minefield.isSafe(5,4));
+        ASSERT_FALSE(minefield.isSafe(5,5));
+
+        ASSERT_TRUE(minefield.isSafe(0,1));
+        ASSERT_TRUE(minefield.isSafe(1,0));
+        ASSERT_FALSE(minefield.isSafe(0,0));
+}
+TEST(FieldTest, checkAdjecent)
+{
+	Field minefield;
 }
